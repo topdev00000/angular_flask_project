84d3d41572d09df295af7a5e8aa72470c9c5cebc
topdev00000 <adrian.gortopan@gmail.com>
2018-05-22 00:47:00 +0900

    fix/backend_2

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ def predict():
     
     # get object from request
     X = request.get_json()
-    # X = [[float(X['postsittingsystolicbp']), float(X['a_g_ratio']), float(X['albumin']), 
+    X = [[float(X['postsittingsystolicbp']), float(X['a_g_ratio']), float(X['albumin']), 
         float(X['creatinine']),float(X['hemoglobin']), float(X['ldh_total']),
         float(X['lymphocytes']), float(X['rbc']),float(X['rdw']),
         float(X['spkt_v_total']),float(X['ufr']),float(X['z_scored_weight'])]]
